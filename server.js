ca8ae216861a5f5f8eaf9451f3fa1af1fde5df74
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-14 23:15:28 -0500

    Add error handling middleware

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Agile Sprint Tracker API!');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
