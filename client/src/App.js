e0389f51fe52287ca41b19e0ab34db86586ecf01
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-14 23:02:48 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Agile Sprint Tracker</h1>
+      <p>Track your sprints effectively!</p>
+    </div>
+  );
+}
+
+export default App;
